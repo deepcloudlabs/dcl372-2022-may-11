8b8d47b93633ac359b8cd9942d049dd508c27724
deepcloudlabs <deepcloudlabs@gmail.com>
2022-05-11 14:31:07 +0300

    initial commit

---
+++ imdb/src/main/java/com/example/imdb/ImdbApplication.java
@@ -0,0 +1,33 @@
+package com.example.imdb;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.example.service.MovieRepository;
+
+@SpringBootApplication
+@ComponentScan({
+	"com.example.service"
+})
+public class ImdbApplication implements ApplicationRunner {
+	private MovieRepository movieRepository;
+	
+	public ImdbApplication(MovieRepository movieRepository) {
+		this.movieRepository = movieRepository;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdbApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		movieRepository.findAllMoviesByGenre("Comedy")
+		               .forEach(System.err::println);
+		
+	}
+
+}
